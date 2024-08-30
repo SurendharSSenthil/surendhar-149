fcdc0dd7d351a553b428e9dc2a2853deb856dcee
SurendharSSenthil <surendharsenthil257@gmail.com>
2024-08-30 07:29:18 +0530

    email verifier

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+const readline = require("readline");
+
+function isValidEmail(email) {
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return emailRegex.test(email);
+}
+
+function validateUserEmail() {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+
+	rl.question("Please enter your email: ", (email) => {
+		if (isValidEmail(email)) {
+			console.log("The email address is valid.");
+		} else {
+			console.log("The email address is invalid.");
+		}
+		rl.close();
+	});
+}
+
+validateUserEmail();
